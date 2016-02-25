413f61ab39b6d3ddd9216453025c6d5eb4a379ba
Kieran Gillibrand <Kieran.Gillibrand6@gmail.com>
2016-02-25 17:12:14 -0500

    Fixes

--- PCBulid/src/login/Login.java
+++ PCBulid/src/login/Login.java
@@ -4,10 +4,8 @@ import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 import java.sql.Connection;  
-import java.sql.DriverManager;  
 import java.sql.PreparedStatement;  
 import java.sql.ResultSet;  
-import java.sql.SQLException;
 import java.util.Random;
 
 import javax.crypto.SecretKeyFactory;
@@ -15,17 +13,10 @@ import javax.crypto.spec.PBEKeySpec;
   
 public class Login 
 {  
-    public static ResultSet validate (String userEmail, String userPassword) 
+    public static ResultSet validate (String userEmail, String userPassword, Connection connection) 
     {          
-        Connection connection = null;  
         PreparedStatement select = null;  
-        ResultSet results = null;  
-  
-        String url = "jdbc:mysql://localhost:3306/";  
-        String dbName = "Prestige Computers";  
-        String driver = "com.mysql.jdbc.Driver";  
-        String userName = "root";  
-        String password = "admin";  
+        ResultSet results = null;
         
         Random random = new Random ();
         byte [] salt = new byte [16];
@@ -56,10 +47,7 @@ public class Login
         
         try 
         {  
-            Class.forName (driver).newInstance ();  
-            connection = DriverManager.getConnection (url + dbName, userName, password);  
-  
-            select = connection.prepareStatement ("select * from login where user=? and password=?");  
+            select = connection.prepareStatement ("select * from users where email=? and password=?");  
             select.setString (1, userEmail);  
             select.setBytes (2, hash);
   
@@ -69,41 +57,6 @@ public class Login
         {  
             System.out.println (e);  
         } 
-        finally 
-        {  
-            if (connection != null) 
-            {  
-                try 
-                {  
-                    connection.close ();  
-                } 
-                catch (SQLException e) 
-                {  
-                    e.printStackTrace ();  
-                }  
-            }  
-            if (select != null) 
-            {  
-                try 
-                {  
-                    select.close ();  
-                } catch (SQLException e) 
-                {  
-                    e.printStackTrace ();  
-                }  
-            }  
-            if (results != null) 
-            {  
-                try 
-                {  
-                    results.close ();  
-                } 
-                catch (SQLException e) 
-                {  
-                    e.printStackTrace ();  
-                }  
-            }  
-        }  
         return results;  
     }  
 } 
--- PCBulid/src/login/LoginServlet.java
+++ PCBulid/src/login/LoginServlet.java
@@ -2,9 +2,13 @@ package login;
   
 import java.io.IOException;  
 import java.io.PrintWriter;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.sql.DriverManager;  
 
 import javax.servlet.RequestDispatcher;  
 import javax.servlet.ServletException;  
@@ -12,6 +16,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;  
 import javax.servlet.http.HttpServletResponse;  
 import javax.servlet.http.HttpSession;
+
 
 import user.User;  
   
@@ -33,10 +38,18 @@ public class LoginServlet extends HttpServlet
   
         System.out.println ("In LoginServlet, Email: " + email + ", Password: " + password);
         
-        ResultSet results = Login.validate (email, password);
-        
         try
         {
+        	String url = "jdbc:mysql://localhost:3306/";  
+            String dbName = "PrestigeComputers"; 
+            String driver = "com.mysql.jdbc.Driver";  
+            String dbUserName = "root";  
+            String dbPassword = "";  
+            
+        	Class.forName (driver).newInstance ();  
+            Connection connection = DriverManager.getConnection (url + dbName, dbUserName, dbPassword);  
+            
+            ResultSet results = Login.validate (email, password, connection);
         	//Correct username/password
         	if (results.next ())
         	{ 
@@ -46,6 +59,8 @@ public class LoginServlet extends HttpServlet
 			
 	        	session.setAttribute ("user", new User (email, firstName, lastName, created));
         	
+	        	System.out.println ("User Logged In: " + ((User) session.getAttribute ("user")).toString ());
+	        	
 	        	RequestDispatcher dispatcher = request.getRequestDispatcher ("welcome.jsp");    
 	        	dispatcher.forward (request, response);    
         	}    
@@ -55,12 +70,25 @@ public class LoginServlet extends HttpServlet
         		out.print ("<p style=\"color:red\">Sorry username or password error</p>");    
         		RequestDispatcher dispatcher = request.getRequestDispatcher ("index.jsp");    
         		dispatcher.include (request, response);    
-        	}    
+        	}   
+        	results.close();
         	out.close();  
         }
         catch (SQLException e)
         {
         	e.printStackTrace ();
         }
+        catch (ClassNotFoundException e) 
+        {
+			e.printStackTrace();
+		} 
+        catch (InstantiationException e) 
+        {
+			e.printStackTrace();
+		} 
+        catch (IllegalAccessException e) 
+        {
+			e.printStackTrace();
+		}
     }    
 }   
