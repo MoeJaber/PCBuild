4fb4e22f9422612bea99781e9eb378d0476604a2
Mouha <Mouha@192.168.2.29>
2016-02-25 16:22:38 -0500

    JSP Table of Database

--- PCBulid/WebContent/cases.jsp
+++ PCBulid/WebContent/cases.jsp
@@ -1,6 +1,8 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"  
-    pageEncoding="ISO-8859-1"%>  
-    <%@page import= "parts_servlet.CasesServlet" %>
+<%@page import="java.sql.DriverManager"%>
+<%@page import="java.sql.ResultSet"%>
+<%@page import="java.sql.Statement"%>
+<%@page import="java.sql.Connection"%>
+<%@page import= "parts_servlet.CasesServlet" %>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -207,8 +209,59 @@ body{
 		</div>
 	</div>
 	
-	 <%=request.getAttribute("str1") %>
-     <%=request.getAttribute("str2") %>
+	 <%
+String id = request.getParameter("userId");
+String driverName = "com.mysql.jdbc.Driver";
+String connectionUrl = "jdbc:mysql://localhost:3306/";
+String dbName = "form";
+String userId = "root";
+String password = "password";
+
+try {
+Class.forName(driverName);
+} catch (ClassNotFoundException e) {
+e.printStackTrace();
+}
+
+Connection connection = null;
+Statement statement = null;
+ResultSet resultSet = null;
+%>
+<h2 align="center"><font><strong>Retrieve data from database in jsp</strong></font></h2>
+<table align="center" cellpadding="5" cellspacing="5" border="1">
+<tr>
+
+</tr>
+<tr >
+<td><b>Model</b></td>
+<td><b>Name</b></td>
+<td><b>Price</b></td>
+</tr>
+<%
+try{ 
+connection = DriverManager.getConnection(connectionUrl+dbName, userId, password);
+statement=connection.createStatement();
+String sql ="SELECT * FROM pc_cases";
+
+resultSet = statement.executeQuery(sql);
+while(resultSet.next()){
+%>
+<tr >
+
+<td><%=resultSet.getString("cases_model") %></td>
+<td><%=resultSet.getString("cases_name") %></td>
+<td><%=resultSet.getInt("cases_price") %></td>
+
+</tr>
+
+<% 
+}
+
+} catch (Exception e) {
+e.printStackTrace();
+}
+%>
+</table>
 	
 	
 <!--- Deals --->
--- PCBulid/src/parts_servlet/CasesServlet.java
+++ PCBulid/src/parts_servlet/CasesServlet.java
@@ -16,102 +16,101 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
- 
+
+import com.mysql.jdbc.Statement;
+import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
 @WebServlet("/JdbcConnection")
 public class CasesServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	 Connection connection = null;
-	 ResultSet resultSet = null;
-	 PreparedStatement preparedStatement = null;
-	 String query = null;
-	 String url = null;
-	 String username = null;
-	 String password = null;
-	
- 
- public void init(ServletConfig config) throws ServletException {
- 
-  url = "jdbc:mysql://localhost:3306/form";
-  username = "root"; 
-  password = "password"; 
-  try {
-   Class.forName("com.mysql.jdbc.Driver").newInstance();
- 
-   connection = DriverManager.getConnection(url, username , password);
-  }
-  catch (Exception e) {
- 
-   e.printStackTrace();
-  }
- 
- }
- 
- protected void doGet(
-   HttpServletRequest request, 
-   HttpServletResponse response
-   ) throws ServletException, IOException {
- 
-  doPost(request, response);
- 
- }   
- 
- protected void doPost(
-   HttpServletRequest request, 
-   HttpServletResponse response
-   ) throws ServletException, IOException {
- 
-  response.setContentType("text/html;charset=UTF-8");
-  PrintWriter out = response.getWriter();
- 
-  query = "SELECT * FROM pc_cases";
-  try {
-   preparedStatement = connection.prepareStatement(query);
-   resultSet = preparedStatement.executeQuery();
- 
-   out.println("<html><body><h2>The Select query has following results : </h2>");
-   out.println("<hr></br><table cellspacing='0' cellpadding='5' border='1'>");
-   out.println("<tr>");
-   out.println("<td><b>Model</b></td>");
-   out.println("<td><b>Name</b></td>");
-   out.println("<td><b>Price</b></td>");
-   out.println("</tr>");
- 
-   while(resultSet.next()) {
-    out.println("<tr>");
-    out.println("<td>"+resultSet.getString(1) + "</td>");
-    out.println("<td>"+resultSet.getString(2) + "</td>");
-    out.println("<td>"+resultSet.getString(3) + "</td>");
-    out.println("</tr>");
-    request.setAttribute("str1", "Test1");
-    request.setAttribute("str2", "Test2");
- 
-   }
-   RequestDispatcher rd=request.getRequestDispatcher("cases.jsp");    
-   rd.forward(request,response);    
-   out.println("</table></br><hr></body></html>");
-  }
-  catch (SQLException e) {
- 
-   e.printStackTrace();
-  }
-   
- }
- 
-@Override
-public void destroy() {
-    try {               
-        resultSet.close();
-                preparedStatement.close();
-    }
-    catch (SQLException e) {
- 
-        e.printStackTrace();
-    }   
- 
-}
- 
- 
-}
+	Connection connection = null;
+	ResultSet resultSet = null;
+	PreparedStatement preparedStatement = null;
+	String query = null;
+	String url = null;
+	String username = null;
+	String password = null;
 
 
-  
+	public void init(ServletConfig config) throws ServletException {
+
+	url = "jdbc:mysql://localhost:3306/form";
+	username = "root"; 
+	password = "password"; 
+	try {
+	Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+	connection = DriverManager.getConnection(url, username , password);
+	}
+	catch (Exception e) {
+
+	e.printStackTrace();
+	}
+
+	}
+
+	protected void doGet(
+	HttpServletRequest request, 
+	HttpServletResponse response
+	) throws ServletException, IOException {
+
+	doPost(request, response);
+
+	}   
+
+	protected void doPost(
+	HttpServletRequest request, 
+	HttpServletResponse response
+	) throws ServletException, IOException {
+
+	response.setContentType("text/html;charset=UTF-8");
+	PrintWriter out = response.getWriter();
+
+	query = "SELECT * FROM pc_cases";
+	try {
+	preparedStatement = connection.prepareStatement(query);
+	resultSet = preparedStatement.executeQuery();
+
+	out.println("<html><body><h2>The Select query has following results : </h2>");
+	out.println("<hr></br><table cellspacing='0' cellpadding='5' border='1'>");
+	out.println("<tr>");
+	out.println("<td><b>Model</b></td>");
+	out.println("<td><b>Name</b></td>");
+	out.println("<td><b>Price</b></td>");
+	out.println("</tr>");
+
+	while(resultSet.next()) {
+	out.println("<tr>");
+	out.println("<td>"+resultSet.getString(1) + "</td>");
+	out.println("<td>"+resultSet.getString(2) + "</td>");
+	out.println("<td>"+resultSet.getString(3) + "</td>");
+	out.println("</tr>");
+	request.setAttribute("str1", "Test1");
+	request.setAttribute("str2", "Test2");
+
+	}
+	 
+	out.println("</table></br><hr></body></html>");
+	}
+	catch (SQLException e) {
+
+	e.printStackTrace();
+	}
+
+	}
+
+	@Override
+	public void destroy() {
+	try {               
+	   resultSet.close();
+	           preparedStatement.close();
+	}
+	catch (SQLException e) {
+
+	   e.printStackTrace();
+	}   
+
+	}}
